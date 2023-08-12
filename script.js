55084e3a3b28e0f7b4bc252be5230fdec284cf46
winnieSikinyi <winniesikinyi@gmail.com>
2023-08-12 10:13:50 +0300

    activated links to my personal platforms

--- script.js
+++ script.js
@@ -24,14 +24,14 @@ $(document).ready(function(){
   });
 
   var typed = new Typed(".typing",{
-    strings: ["Developer", "Blogger","Designer"],
+    strings: ["Developer", "Product Manager","UX Researcher"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
   });
 
   var typed = new Typed(".typing-2",{
-    strings: ["Developer", "Blogger","Designer"],
+    strings: ["Developer", "Product Manager","UX Researcher"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
